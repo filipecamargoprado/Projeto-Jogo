03b28dc05ec142d9320d66508f94461f32c40461
GuilhermeAmi <guilherme.ami@eletrorastro.com.br>
2024-10-20 00:40:51 -0300

    Correção Program.cs

--- ProjetoJogo/Program.cs
+++ ProjetoJogo/Program.cs
@@ -8,7 +8,7 @@ var app = builder.Build();
 // Rota inicial que retorna um HTML simples
 app.MapGet("/", () => 
 {
-    var cenario = Cenario1();
+    var cenario = new Cenario1();
 
 
     var html = $@"
@@ -45,3 +45,4 @@ app.MapPost("/escolher", async (HttpContext context) =>
 
 
 app.Run();
+
